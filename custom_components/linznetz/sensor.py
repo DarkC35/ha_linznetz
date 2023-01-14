bd3ab5b552a4bb91a7a72f520f5dade442ef7342
DarkC <38973046+DarkC35@users.noreply.github.com>
2023-01-14 14:15:22 +0000

    add basic tests

--- custom_components/linznetz/sensor.py
+++ custom_components/linznetz/sensor.py
@@ -34,6 +34,7 @@ from .const import (
     DEFAULT_NAME,
     DOMAIN,
     SERVICE_IMPORT_REPORT,
+    START_TIME_KEY,
 )
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
@@ -56,6 +57,11 @@ async def async_setup_entry(
     async_add_devices([LinzNetzSensor(config_entry)])
 
 
+def get_csv_data_value_key(csv_data: list) -> str:
+    """Gets the key to access the value property from a given csv_data list."""
+    return list(csv_data[0].keys())[2]
+
+
 def parse_csv_date_str(csv_date_str: str) -> datetime:
     """Parses the Austrian time string to an UTC datetime."""
     parsed_str = dt_util.as_utc(
@@ -64,6 +70,27 @@ def parse_csv_date_str(csv_date_str: str) -> datetime:
         )
     )
     return parsed_str
+
+
+def parse_german_number_str_to_decimal(number_str: str) -> Decimal:
+    """Parses a German number string from the CSV to a Decimal."""
+    return Decimal(number_str.replace(",", "."))
+
+
+def parse_value_to_decimal(value) -> Decimal:
+    """Parses a value to a decimal with floating point error workaround."""
+    return Decimal(str(value))
+
+
+def get_csv_data_list_from_file(file_path: str):
+    """Returns content on file as csv list."""
+    if not os.path.isfile(file_path):
+        raise HomeAssistantError(f"Report file at path {file_path} not found.")
+    with open(file_path, encoding="UTF-8") as file:
+        _LOGGER.debug(file)
+        report_dict_reader = csv.DictReader(file, delimiter=";")
+        csv_data = list(report_dict_reader)
+    return csv_data
 
 
 class LinzNetzSensor(SensorEntity):
@@ -98,105 +125,97 @@ class LinzNetzSensor(SensorEntity):
             self.unique_id,
         )
 
-        if not os.path.isfile(path):
-            raise HomeAssistantError(f"Report file at path {path} not found.")
+        # metadata for external stats
+        # metadata = StatisticMetaData(
+        #     unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        #     state_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        #     source=DOMAIN,
+        #     name=self.name,
+        #     statistic_id=self.statistic_id,
+        #     has_mean=False,
+        #     has_sum=True,
+        # )
+        # metadata for internal stats
+        metadata = StatisticMetaData(
+            unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+            # state_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+            source="recorder",
+            name=self.name,
+            statistic_id=self.entity_id,
+            has_mean=False,
+            has_sum=True,
+        )
+        statistics = []
 
-        with open(path, encoding="UTF-8") as file:
-            _LOGGER.debug(file)
-            # metadata for external stats
-            # metadata = StatisticMetaData(
-            #     unit_of_measurement=ENERGY_KILO_WATT_HOUR,
-            #     state_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
-            #     source=DOMAIN,
-            #     name=self.name,
-            #     statistic_id=self.statistic_id,
-            #     has_mean=False,
-            #     has_sum=True,
-            # )
-            # metadata for internal stats
-            metadata = StatisticMetaData(
-                unit_of_measurement=ENERGY_KILO_WATT_HOUR,
-                # state_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
-                source="recorder",
-                name=self.name,
-                statistic_id=self.entity_id,
-                has_mean=False,
-                has_sum=True,
+        csv_data = get_csv_data_list_from_file(path)
+
+        if len(csv_data) % 96 != 0:
+            raise HomeAssistantError(
+                "Report to import should have exactly 96 quarter-hour entries per day."
             )
-            statistics = []
 
-            report_dict_reader = csv.DictReader(file, delimiter=";")
-            csv_data = list(report_dict_reader)
+        last_inserted_stat = await get_instance(self.hass).async_add_executor_job(
+            get_last_statistics, self.hass, 1, self.entity_id, True, {"sum"}
+        )
+        inserted_stats = {self.entity_id: []}
+        _LOGGER.debug("Last inserted stat:")
+        _LOGGER.debug(last_inserted_stat)
 
-            if len(csv_data) % 96 != 0:
-                raise HomeAssistantError(
-                    "Report to import should have exactly 96 quarter-hour entries per day."
-                )
-
-            last_inserted_stat = await get_instance(self.hass).async_add_executor_job(
-                get_last_statistics, self.hass, 1, self.entity_id, True, {"sum"}
+        if len(last_inserted_stat) == 0 or len(last_inserted_stat[self.entity_id]) == 0:
+            _sum = Decimal(0)
+            _LOGGER.debug("No previous inserted stats, start sum with 0.")
+        elif (
+            len(last_inserted_stat) == 1
+            and len(last_inserted_stat[self.entity_id]) == 1
+            and last_inserted_stat[self.entity_id][0]["start"]
+            < parse_csv_date_str(csv_data[0][START_TIME_KEY])
+        ):
+            _sum = parse_value_to_decimal(last_inserted_stat[self.entity_id][0]["sum"])
+            _LOGGER.debug("Previous inserted stats found, start sum with %f.", _sum)
+        else:
+            inserted_stats = await get_instance(self.hass).async_add_executor_job(
+                statistics_during_period,
+                self.hass,
+                parse_csv_date_str(csv_data[0][START_TIME_KEY]) - timedelta(hours=1),
+                None,
+                [self.entity_id],
+                "hour",
+                None,
+                {"sum", "state"},
             )
-            inserted_stats = {self.entity_id: []}
-            _LOGGER.debug("Last inserted stat:")
-            _LOGGER.debug(last_inserted_stat)
+            _LOGGER.debug("Inserted stats:")
+            _LOGGER.debug(inserted_stats)
+            _sum = (
+                parse_value_to_decimal(inserted_stats[self.entity_id][0]["sum"])
+                if len(inserted_stats) > 0
+                and len(inserted_stats[self.entity_id]) > 0
+                and inserted_stats[self.entity_id][0]["start"]
+                < parse_csv_date_str(csv_data[0][START_TIME_KEY])
+                else Decimal(0)
+            )
+            _LOGGER.debug("Overlap detected, start sum with %f.", _sum)
 
-            if (
-                len(last_inserted_stat) == 0
-                or len(last_inserted_stat[self.entity_id]) == 0
-            ):
-                _sum = Decimal(0)
-                _LOGGER.debug("No previous inserted stats, start sum with 0.")
-            elif (
-                len(last_inserted_stat) == 1
-                and len(last_inserted_stat[self.entity_id]) == 1
-                and last_inserted_stat[self.entity_id][0]["start"]
-                < parse_csv_date_str(csv_data[0]["Datum von"])
-            ):
-                _sum = Decimal(last_inserted_stat[self.entity_id][0]["sum"])
-                _LOGGER.debug("Previous inserted stats found, start sum with %f.", _sum)
-            else:
-                inserted_stats = await get_instance(self.hass).async_add_executor_job(
-                    statistics_during_period,
-                    self.hass,
-                    parse_csv_date_str(csv_data[0]["Datum von"]) - timedelta(hours=1),
-                    None,
-                    [self.entity_id],
-                    "hour",
-                    None,
-                    {"sum", "state"},
-                )
-                _LOGGER.debug("Inserted stats:")
-                _LOGGER.debug(inserted_stats)
-                _sum = (
-                    Decimal(inserted_stats[self.entity_id][0]["sum"])
-                    if len(inserted_stats) > 0
-                    and len(inserted_stats[self.entity_id]) > 0
-                    and inserted_stats[self.entity_id][0]["start"]
-                    < parse_csv_date_str(csv_data[0]["Datum von"])
-                    else Decimal(0)
-                )
-                _LOGGER.debug("Overlap detected, start sum with %f.", _sum)
-
-            hourly_sum = Decimal(0)
-            start = None
-            for index, record in enumerate(csv_data, start=1):
-                hourly_sum += Decimal(record[list(record.keys())[2]].replace(",", "."))
-                if index % 4 == 1:
-                    start = parse_csv_date_str(record["Datum von"])
-                if index % 4 == 0:
-                    _sum += hourly_sum
-                    statistics.append(
-                        StatisticData(
-                            start=start,
-                            state=hourly_sum,
-                            sum=_sum,
-                        )
+        hourly_sum = Decimal(0)
+        start = None
+        csv_data_value_key = get_csv_data_value_key(csv_data)
+        for index, record in enumerate(csv_data, start=1):
+            hourly_sum += parse_german_number_str_to_decimal(record[csv_data_value_key])
+            if index % 4 == 1:
+                start = parse_csv_date_str(record[START_TIME_KEY])
+            if index % 4 == 0:
+                _sum += hourly_sum
+                statistics.append(
+                    StatisticData(
+                        start=start,
+                        state=hourly_sum,
+                        sum=_sum,
                     )
-                    hourly_sum = Decimal(0)
+                )
+                hourly_sum = Decimal(0)
         for stat in inserted_stats[self.entity_id]:
             if stat["start"] <= statistics[-1]["start"]:
                 continue
-            _sum += Decimal(stat["state"])
+            _sum += parse_value_to_decimal(stat["state"])
             statistics.append(
                 StatisticData(
                     start=stat["start"],
